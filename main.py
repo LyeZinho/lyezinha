51eddfd783b8bf01d50d2b120eda8669f575e41d
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2021-10-22 16:10:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# bot.py
+import os
+import random
+import json
+from discord.ext import commands
+from dotenv import load_dotenv
+from discord import Embed
+from discord import Colour
+import requests
+
+load_dotenv()
+
+TOKEN = os.environ['TOKEN']
+
+
+bot = commands.Bot(command_prefix='!')
+
+
+
+
+
+@bot.command(name='test', help='something')
+async def nine_nine(ctx):
+  response = requests.get('https://api.waifu.pics/sfw/waifu')
+  imageResponse = response.json()["url"]
+  embed = Embed(
+            title="-🌸Waifu🌸-"
+              )
+  await ctx.send(embed=embed)
+  await ctx.send("{0}".format(imageResponse))
+   
+
+      
+
+bot.run(TOKEN)
